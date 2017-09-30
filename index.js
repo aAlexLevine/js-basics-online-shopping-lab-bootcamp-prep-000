b7a6c2ace9747b3d5840230488640e2615775286
Alex Levine <aalexlevine@gmail.com>
2017-09-30 16:13:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,6 +73,11 @@ function removeFromCart(item)
         }
     }
 
-function placeOrder(cardNumber) {
-  // write your code here
-}
+    function placeOrder(cardNumber){
+      if (cardNumber) {
+        console.log('Your total cost is ' + total() +', which will be charged to the card' + cardNumber+'.');
+      }
+      else {
+        console.log('Sorry, we don\'t have a credit card on file for you.');
+    }
+    }
